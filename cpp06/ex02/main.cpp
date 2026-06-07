7984c224556e68509839dc20084193f63c0fad74
hamzah1432 <halmuhesn@gmail.com>
2026-06-07 13:22:58 +0300

    feat: add Base, A, B, and C classes with corresponding headers and implementation

---
+++ cpp06/ex02/main.cpp
@@ -0,0 +1,80 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: halmuhis <halmuhesn@gmail.com>             +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/07 12:56:46 by halmuhis          #+#    #+#             */
+/*   Updated: 2026/06/07 13:19:28 by halmuhis         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <cstdlib>
+#include <ctime>
+#include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
+#include <iostream>
+
+Base*    generate(void)
+{
+    int random = rand() % 3;
+
+    switch (random)
+    {
+        case 0:
+            return new A();
+        case 1:
+            return new B();
+        case 2:
+            return new C();
+        default:
+            return NULL;
+    }
+}
+
+void identify(Base* p)
+{
+    if (dynamic_cast<A*>(p))
+        std::cout << "A" << std::endl;
+    else if (dynamic_cast<B*>(p))
+        std::cout << "B" << std::endl;
+    else if (dynamic_cast<C*>(p))
+        std::cout << "C" << std::endl;
+}
+
+void identify(Base& p)
+{
+    try {
+        (void)dynamic_cast<A&>(p);
+        std::cout << "A" << std::endl;
+    }
+    catch (std::exception& e) {}
+    try {
+        (void)dynamic_cast<B&>(p);
+        std::cout << "B" << std::endl;
+    }
+    catch (std::exception& e) {}
+    try {
+        (void)dynamic_cast<C&>(p);
+        std::cout << "C" << std::endl;
+    }
+    catch (std::exception& e) {}
+}
+
+
+int main()
+{
+    srand(time(0));
+    Base* base;
+    for (int i = 0; i < 10; i++)
+    {
+        base = generate();
+        identify(base);
+        identify(*base);
+        delete base;
+    }
+    return 0;
+}
